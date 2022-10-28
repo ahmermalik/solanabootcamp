eebca866e1006486b1775b94c9d8c422f6327e5a
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2022-10-28 12:02:33 -0500

    re arrange function location

--- Wk1/fizz_buzz/src/main.rs
+++ Wk1/fizz_buzz/src/main.rs
@@ -1,24 +1,22 @@
+
+fn fizz_buzz(num: i32) {
+    let mut counter = 0;
+    for x in 0..num {
+        if x % 3 == 0 && x % 5 == 0 {
+            println!("fizz buzz");
+            counter = counter + 1;
+        } else if x % 3 == 0 {
+            println!("fizz")
+        } else if x % 5 == 0 {
+            println!("buzz")
+        } else {
+            println!("{} not divisible", x)
+        }
+    }
+    println!("fizz buzz occurred {} times", counter)
+}
+
 fn main() {
     println!("==== Welcome to Fizz Buzz! ====");
-    fn fizz_buzz(num: i32) {
-        let mut counter = 0;
-        for x in 0..num {
-            if x % 3 == 0 && x % 5 == 0 {
-                println!("fizz buzz");
-                counter = counter + 1;
-            }
-            else if x % 3 == 0 {
-                println!("fizz")
-            }
-            else if x % 5 == 0 {
-                println!("buzz")
-            }
-            else {
-                println!("{} not divisible", x)
-            }
-        }
-        println!("fizz buzz occurred {} times", counter)
-    }
-
     fizz_buzz(301)
 }
